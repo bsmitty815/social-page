1e505e83df59232d3a8878683d19a5dd11101f61
Brian Smith <brian.bubbam@gmail.com>
2021-08-30 14:21:58 -0500

    working on app component, sign up component, login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import Home from './components/Home'
+
 import './App.css';
+import FirstComponent from './FirstComponent'
 import { Route, Switch } from "react-router-dom";
+import Login from "./Login"
+import Signup from "./Signup"
+import { useState } from "react"
 
 function App() {
+  const [user, setUser] = useState(null)
+  
   return (
     <div>
-      <Route path="/Home">
-        <Home />
-      </Route>
-
+      <Switch>
+        <Route >
+>
+        </Route>
+      </Switch>
     </div>
   );
 }
